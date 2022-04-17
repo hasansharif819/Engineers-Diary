3d69c5b678aca4119ec9406fbedf5c3b458ce185
hasansharif819 <hs.sharif819@gmail.com>
2022-04-18 03:52:22 +0600

    navbar & carousal problem

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
 import Services from '../Services/Services';
+import Slider from '../Slider/Slider';
 
 const Home = () => {
     return (
         <div>
+            <Banner></Banner>
+            {/* <Slider></Slider> */}
             <Services></Services>
         </div>
     );
---
+++ src/pages/Slider/Slider.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { Carousel } from 'react-bootstrap';
+
+import g1 from '../../Image/slider/g1.jpg';
+import g2 from '../../Image/slider/g2.jpg';
+import g3 from '../../Image/slider/g3.jpg';
+
+const Slider = () => {
+    const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+    return (
+        <Carousel activeIndex={index} onSelect={handleSelect}>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={g1}
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={g2}
+                    alt="Second slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={g3}
+                    alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>
+                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                    </p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+    );
+};
+
+export default Slider;
